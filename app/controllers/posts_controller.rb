7b0d73f7b8eb2167ffe8095aae7709727669d060
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-09 00:21:25 -0600

    added edit, delete functionality, added view partiels to reduce main code size, cleaned up code, added methods to controller, etc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,72 @@
 class PostsController < ApplicationController
 
+  #index for to display all posts by time they were created at.
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
+  #Method for creating a new post.
   def new
     @post = Post.new
   end
 
+  #Method for further creation of a new post.
   def create
     @post = Post.new(post_params)
 
     if @post.save
+
     redirect_to @post
+
     else
+
       render 'new'
+
     end
-end
+  end
+
+  #Method for displaying the post in the view file /posts/show.html.erb.
   def show
+
+    @post = Post.find(params[:id])
+
+  end
+
+  #Method for editing an existing Post.
+  def edit
     @post = Post.find(params[:id])
   end
 
+  #Method for updating and exsisting post that has been edited.
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+
+  end
+
+  #Method to Delete a post.
+  def destroy
+
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
+
+  end
+
+  #Begin of rails 4 private method creation.
   private
 
   def post_params
+
     params.require(:post).permit(:title, :body)
+
   end
+
 end
