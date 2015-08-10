22d5b0f6bb63d29c6a4b5e8b532418db3861db3d
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-09 18:26:16 -0600

    added comments section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,21 @@
 class CommentsController < ApplicationController
 
+  #Method to create a comment.
   def create
     @post = Post.find(params[:post_id])
 
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+    redirect_to post_path(@post)
+  end
+
+  #Method to destroy comments.
+  def destroy
+    @post = Post.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
 
     redirect_to post_path(@post)
   end
